ae6e627898d89d677ebbeca99796c455572c9945
Srikar Kadiyam <srikarkadiyam@Srikars-MacBook-Air.local>
2026-02-18 22:32:52 +0530

    Cleaned cpp file

--- TestApp/qt/qt_app.cpp
+++ TestApp/qt/qt_app.cpp
@@ -251,7 +251,7 @@ inline void PythonConsole::runScript()
         auto locals = py::dict();
         locals["viewer"] = py::cast(viewer, py::return_value_policy::reference);
         // Instead of casting the object directly, cast the address to a long long
-        locals["mainwindow_ptr"] = reinterpret_cast<uintptr_t>(viewer->parent());
+        // locals["mainwindow_ptr"] = reinterpret_cast<uintptr_t>(viewer->parent());
         // locals["mainwindow"] = py::cast(viewer->parent(), py::return_value_policy::reference);
         // globals["gfx"] = gfx;
 
